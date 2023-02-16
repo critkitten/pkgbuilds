f8039d3b8ddb328f8dd310cabfd205687308b478
critkitten <critkitten@nirvanda.aaa>
2023-02-16 17:12:49 +0100

    add swayidle

--- windowmanager/install.sh
+++ windowmanager/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-for f in {wlroots,sway,swaybg}; do cd $f/keys/pgp && gpg --import * && cd ../../..; done
-for f in {wlroots,seatd,sway,swaybg,foot,wl-clipboard}; do cd $f && makepkg -fscCri && cd ..; done
+for f in {wlroots,sway,swaybg,swayidle}; do cd $f/keys/pgp && gpg --import * && cd ../../..; done
+for f in {wlroots,seatd,sway,swaybg,swayidle,foot,wl-clipboard}; do cd $f && makepkg -fscCri && cd ..; done
