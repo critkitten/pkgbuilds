6b77a41cb3eb42a052fb128ca048723931589d63
critkitten <critkitten@nirvanda.aaa>
2023-02-25 19:59:22 +0100

    add mympd to install-mpd.sh script

--- multimedia/install-mpd.sh
+++ multimedia/install-mpd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 for f in {flac,mpd}; do cd $f/keys/pgp && gpg --import * && cd ../../..; done
-for f in {flac,mpd}; do cd $f && makepkg -fscCri && cd ..; done
+for f in {flac,mpd,mympd}; do cd $f && makepkg -fscCri && cd ..; done
